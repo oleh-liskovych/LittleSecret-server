178df0de4a57ad77b8f8c91b98b030d1c5a0a6cd
Oleh <oleh.liskovych@gmail.com>
2019-06-10 14:00:41 +0300

    Basic setup

---
+++ app/__init__.py
@@ -0,0 +1,42 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request, current_app
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_babel import Babel, lazy_gettext as _l
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+babel = Babel()
+
+
+def create_app(config_class=Config):
+    app=Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    babel.init_app(app)
+
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/ls.log', maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.info('LittleSecret')
+
+            return app
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+
+from app import models
+from app.auth import routes
+
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "just-in-case-secret-phrase-is-absent"
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    LANGUAGES = ['en', 'uk', 'ru']
