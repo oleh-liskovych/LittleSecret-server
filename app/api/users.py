37b8c5b8d0b64d47c7d8740f3b371985168c4b0b
Oleh <oleh.liskovych@gmail.com>
2019-06-17 00:33:00 +0300

    Create user request added

--- app/api/users.py
+++ app/api/users.py
@@ -1,6 +1,8 @@
+from flask import jsonify, request, url_for
+from app import db
 from app.api import bp
+from app.api.errors import bad_request
 from app.models import User
-from flask import jsonify
 
 
 @bp.route('/users/<int:id>', methods=['GET'])
@@ -15,7 +17,25 @@ def get_users():
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    pass
+    print("request.get_json()", request.get_json())
+    print("request.get_json()", request)
+    # print("request.get_json()", request.get_json())
+    data = request.get_json() or {}
+    # print('username {}'.format(data['username']), 'email {}'.format(data['email']), 'password {}'.format(data['password']))
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('Must include username, email and password fields')
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request('Please use a different username')
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request('Please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
 
 
 @bp.route('/users/<int:id>', methods=['PUT'])
