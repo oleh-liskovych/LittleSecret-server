82a2d1fc3454e3b0678a8b21368ceb8ca1646782
Oleh <oleh.liskovych@gmail.com>
2019-06-19 19:42:05 +0300

    Create user route via multipart added. Docs updated

--- app/api/users.py
+++ app/api/users.py
@@ -1,6 +1,6 @@
-from flask import jsonify, request, make_response, url_for, g, abort, current_app
+from flask import jsonify, request, url_for, g, abort, current_app
 from app import db
-from app.common.utils import deprecated, allowed_file
+from app.common.utils import allowed_file
 from app.api import bp
 from app.api.errors import bad_request
 from app.models import User
@@ -26,13 +26,20 @@ def get_users():
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    data = request.get_json() or {}
+    data = request.form.to_dict() or {}
     if 'username' not in data or 'email' not in data or 'password' not in data:
         return bad_request('Must include username, email and password fields')
     if User.query.filter_by(username=data['username']).first():
         return bad_request('Please use a different username')
     if User.query.filter_by(email=data['email']).first():
         return bad_request('Please use a different email address')
+    if 'picture' in request.files:
+        picture = request.files["picture"]
+        if picture and allowed_file(picture.filename):
+            filename = secure_filename(picture.filename)
+            path = os.path.join(current_app.config['UPLOADS'], filename)
+            picture.save(path)
+            data['picture'] = url_for('common.send_file', filename=filename, _external=True)
     user = User()
     user.from_dict(data, new_user=True)
     db.session.add(user)
@@ -43,25 +50,9 @@ def create_user():
     return response
 
 
-@deprecated
-@bp.route('/users/<string:username>/deprecated', methods=['PUT'])
-@token_auth.login_required
-def update_user(username):
-    if g.current_user.username != username:
-        abort(403)
-    user = User.query.filter_by(username=username).first_or_404()
-    data = request.get_json() or {}
-    if 'email' in data and data['email'] != user.email and \
-            User.query.filter_by(email=data['email']).first():
-        return bad_request('Please use different email address')
-    user.from_dict(data, new_user=False)
-    db.session.commit()
-    return jsonify(user.to_dict())
-
-
 @bp.route('/users/<string:username>', methods=['PUT'])
 @token_auth.login_required
-def update_user_multipart(username):
+def update_user(username):
     if g.current_user.username != username:
         abort(403)
     user = User.query.filter_by(username=username).first_or_404()
@@ -79,15 +70,3 @@ def update_user_multipart(username):
     user.from_dict(data, new_user=False)
     db.session.commit()
     return jsonify(user.to_dict())
-
-
-@bp.route('/users/upload', methods=['POST'])
-def test_upload():
-    name = request.form.get("name")
-    image = request.files["image"]
-    print(image)
-    image.save(os.path.join(current_app.config['UPLOADS'], image.filename))
-    print("Image saved")
-    response = make_response(str(name) + " loh", 200)
-    response.mimetype = "text/plain"
-    return response
